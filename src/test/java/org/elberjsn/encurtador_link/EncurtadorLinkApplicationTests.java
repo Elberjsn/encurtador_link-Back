87725737fc7a5a7481d194fc46065ee76ab85e8f
elberjsn <elberjsn@gmail.com>
2025-06-01 11:37:24 -0300

    Start Project

---
+++ src/test/java/org/elberjsn/encurtador_link/EncurtadorLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elberjsn.encurtador_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
